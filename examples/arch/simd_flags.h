d2c30607f73d0888624e7be03cf692ae615d87bf
William Yang <williamyang2017@gmail.com>
2023-07-11 03:41:54 +1000

    Avoid macro redefinition warning on MSVC

--- examples/arch/simd_flags.h
+++ examples/arch/simd_flags.h
@@ -5,26 +5,28 @@
 #if defined(__ARCH_X86__)
     #if defined(_MSC_VER)
         // We do this to fix missing flags on compilers like MSVC so they match gcc and clang
-        #if defined(__AVX512__)
+        #if defined(__AVX512__) && !defined(__AVX2__)
             #define __AVX2__
         #endif
-        #if defined(__AVX2__)
+        #if defined(__AVX2__) && !defined(__AVX__)
             #define __AVX__
         #endif
-        #if defined(__AVX__) 
+        #if defined(__AVX__) && !defined(__SSE4_2__)
             #define __SSE4_2__
+        #endif
+        #if defined(__AVX__) && !defined(__FMA__)
             #define __FMA__
         #endif
-        #if defined(__SSE4_2__)
+        #if defined(__SSE4_2__) && !defined(__SSE4_1__)
             #define __SSE4_1__
         #endif
-        #if defined(__SSE4_1__)
+        #if defined(__SSE4_1__) && !defined(__SSE3__)
             #define __SSE3__
         #endif
-        #if defined(__SSE3__)
+        #if defined(__SSE3__) && !defined(__SSE2__)
             #define __SSE2__
         #endif
-        #if defined(__SSE2__)
+        #if defined(__SSE2__) && !defined(__SSE__)
             #define __SSE__
         #endif
     #endif
